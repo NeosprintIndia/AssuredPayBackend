480c1350a6c7f9add5061a468fdcd23f9927ad5d
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-08-24 18:24:16 +0530

    update

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-const express=require("express");
-const app= express();
+const express = require("express");
+const app = express();
 
-app.get("/",(req,res)=>{
-  console.log("test api")
-    res.send(200).json("Exciting News: AssuredPay Project is Set to Transform the Future of Business Finance!");
+app.get("/", (req, res) => {
+  console.log("test api");
+  res.status(200).json("Exciting News: AssuredPay Project is Set to Transform the Future of Business Finance!");
+});
 
-})
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
