bcaf6f1b898bb67bb5dacf76ceb5d76a3a643e89
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-08-24 18:30:34 +0530

    update

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req:any, res:any) => {
   console.log("test api");
   res.status(200).json("Exciting News: AssuredPay Project is Set to Transform the Future of Business Finance!");
 });
