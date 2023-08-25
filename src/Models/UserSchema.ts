5bb23a5afcdce5a500a2c9553763d5c8154a39d2
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-08-25 19:19:17 +0530

    User Model  Created
    User Model Created along with the post api to test user model

---
+++ src/Models/UserSchema.ts
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    accountNumber: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now }
+})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,44 @@
 import express from 'express';
 const app = express();
+const User = require("./Models/UserSchema");
+const mongoose = require("mongoose");
+var bodyParser = require('body-parser')
+
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+mongoose.connect("mongodb+srv://nitinkumar2610:Snitin16@testdb.vyf0wul.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
 });
+
+app.get('/', (req:any, res:any) => {
+  res.send('Exciting News: AssuredPay Project is Set to Transform the Future of Business Finance!');
+});
+
+app.post('/users', async(req: any, res: any) => {
+  try {
+ 
+    console.log(req.body);
+    const newUser = req.body; 
+    
+     const savedUser = await User.create(newUser); 
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}); 
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
