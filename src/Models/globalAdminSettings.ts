07be82a23da7641959dd349f393b0aea6a80d13e
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-09-15 19:06:24 +0530

    Update globalAdminSettings.ts

--- src/Models/globalAdminSettings.ts
+++ src/Models/globalAdminSettings.ts
@@ -1,49 +1,49 @@
-import mongoose, { Schema, Document, Model, Types } from 'mongoose';
+// import mongoose, { Schema, Document, Model, Types } from 'mongoose';
 
-// Define the interface for the document
-interface IReferralCode extends Document {
-user: Types.ObjectId | IUser;
-gstLimit: number;
-aadharLimit: number;
-panLimit:number;
-enrollmentFees:number;
-cin:number;
-termsOfService:string;
-privacyPolicy:string;
-disclaimer:string;
-}  
+// // Define the interface for the document
+// interface IReferralCode extends Document {
+// user: Types.ObjectId | IUser;
+// gstLimit: number;
+// aadharLimit: number;
+// panLimit:number;
+// enrollmentFees:number;
+// cin:number;
+// termsOfService:string;
+// privacyPolicy:string;
+// disclaimer:string;
+// }  
 
-// Define the reference interface for the user field
-interface IUser extends Document { _id: Types.ObjectId; }
+// // Define the reference interface for the user field
+// interface IUser extends Document { _id: Types.ObjectId; }
 
-// Define the schema
-const globalSettingSchema: Schema<IReferralCode> = new Schema<IReferralCode>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'RegisterUser',
-  },
-  gstLimit: {
-    type: Number,
-  },
-  aadharLimit: {
-    type: Number,
-},
-panLimit: {
-    type: Number,
-},
-cin: {
-    type: Number,
-},
-enrollmentFees: {
-    type: Number,
-},
-termsOfService:{type:String},
-privacyPolicy:{type:String},
-disclaimer:{type:String},
+// // Define the schema
+// const globalSettingSchema: Schema<IReferralCode> = new Schema<IReferralCode>({
+//   user: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'RegisterUser',
+//   },
+//   gstLimit: {
+//     type: Number,
+//   },
+//   aadharLimit: {
+//     type: Number,
+// },
+// panLimit: {
+//     type: Number,
+// },
+// cin: {
+//     type: Number,
+// },
+// enrollmentFees: {
+//     type: Number,
+// },
+// termsOfService:{type:String},
+// privacyPolicy:{type:String},
+// disclaimer:{type:String},
 
-});
+// });
 
-// Define the model
-const adminGlobalSetting: Model<IReferralCode> = mongoose.model<IReferralCode>('User_Coupon_Code', globalSettingSchema);
+// // Define the model
+// const adminGlobalSetting: Model<IReferralCode> = mongoose.model<IReferralCode>('User_Coupon_Code', globalSettingSchema);
 
-export default adminGlobalSetting;
+// export default adminGlobalSetting;
