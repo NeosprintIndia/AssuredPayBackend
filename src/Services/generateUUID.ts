a09cb909f5dd8c1b229b93aae3c8c2e564218e3b
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-09-26 15:47:43 +0530

    deleted all files and folder
    deleted all files and folder

--- src/Services/generateUUID.ts
+++
@@ -1,9 +0,0 @@
-
-import { v4 as uuidv4 } from 'uuid';
-
-function generateUUID(): string {
-  const generatedUUID = uuidv4().replace(/-/g, '').substring(0, 12);
-  return generatedUUID;
-}
-
-export { generateUUID };
