d31fee6f101e97b8ca86eff6d74f2802dbf5d9dd
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-09-26 14:26:18 +0530

    Log Error Resolve

--- src/Controller/admin/templateController.ts
+++ src/Controller/admin/templateController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import TemplateDB from '../../models/templateSES'
-import fs from 'fs';
 import json2csv from 'json2csv';
 import csv from 'csv-parser';
 
