57a8b56acb1c7e153026424cb0d8a2efbda5a257
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-10-31 13:10:29 +0530

    Update sendSMS.ts

--- src/services/sendSMS.ts
+++ src/services/sendSMS.ts
@@ -6,15 +6,20 @@ export const sendSMS = async (data:any) => {
     try {
     
         const templateDoc = await findAlertsTemplate(data.Message_slug);
-
         const message: string = replaceVarsInSequence(templateDoc.data.Message, data.VariablesMessage);
-        const encodedMessage =  encodeURIComponent(message);       
-        const encodedPassword = encodeURIComponent(process.env.Route_Mobile_SMS_PASSWORD);
-       
-
+        const customParamsSerializer = params => {
+            return Object.keys(params)
+              .map(key => {
+                return `${key}=${encodeURIComponent(params[key])}`;
+              })
+              .join('&');
+          };
         const instance = axios.create({
-            baseURL: "http://sms6.rmlconnect.net:8080",
+            baseURL: "https://sms6.rmlconnect.net",
         });
+
+        console.log(process.env.Route_Mobile_SMS_PASSWORD)
+        console.log(process.env.Route_Mobile_SMS_USERID)
 
         const config: AxiosRequestConfig = {
             method: 'GET',
@@ -23,20 +28,21 @@ export const sendSMS = async (data:any) => {
                 'Content-Type': 'application/json',
             },
             params: {
-                username: process.env.Route_Mobile_SMS_USERID,
-                password: encodedPassword,
-                type: "0",
-                dlr: "1",
+                username:process.env.Route_Mobile_SMS_USERID,
+                password:process.env.Route_Mobile_SMS_PASSWORD,
+                type:"0",
+                dlr:"1",
                 destination: data.receiverNo,
                 source: "ASRDPY",
-                message:encodedMessage,
+                message:message,
                 entityid:"1401743190000042074",
                 tempid:templateDoc.data.Template_ID,   
             },
+            paramsSerializer: customParamsSerializer,
         };
         console.log("config",config)
         const response = await instance.request(config);
-        console.log("response",response.data)
+        console.log("response",response)
         return response.data;
     } catch (error) {
         console.error(error);
