f250120755f2690efe199e43b7c19b51ca1d4409
Nitin29121995 <143087117+Nitin29121995@users.noreply.github.com>
2023-12-15 10:49:05 +0530

    Update userKYCs.ts

--- src/models/userKYCs.ts
+++ src/models/userKYCs.ts
@@ -246,7 +246,7 @@ const UserKYCSchema: Schema<IUserKYC> = new Schema<IUserKYC>(
     due: {
       type: String,
       default: "New",
-      enum: ["New", "Approved","Rejected","reAsked"] 
+      enum: ["New", "Approved","Rejected","reUpload"] 
     },
   },
   {
